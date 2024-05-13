d111bd183aaac2f86853ebb7b97d9ea879a4ea65
Paul Stinchcombe <paul.stinchcombe@gmail.com>
2024-05-13 15:38:49 +0800

    Update

--- index.ts
+++ index.ts
@@ -1,18 +1,25 @@
-import StateMachine, { State, EventListener, NextAction } from 'ocustate';
-import path from 'path';
+import { StateMachine, State, EventListener, NextAction } from 'ocustate';
+import Express from 'express';
+const app = Express();
+app.use(Express.json());
+app.use(Express.static('public'));
+
+app.listen(3000, () => {
+	console.log('Example app listening on port 3000!');
+});
 
 class Machine implements EventListener {
 	async setMachinePath(forState: string) {
 		console.log(`Get StateMachine from state of ${forState}`);
-		return path.resolve(__dirname, 'states', 'states.json');
+		return 'http://localhost:3000/states/states.json';
 	}
 
 	async executeState(state: State) {
-		console.log('Executing state' + state.name);
+		console.log(`Executing state ${state.name}`);
 		return NextAction.NoOp;
 	}
 }
 
 const myMachine = new Machine();
 StateMachine.AddEventListener(myMachine);
-StateMachine.Execute('state');
+StateMachine.Execute('Draft');
